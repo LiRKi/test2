9bce6ef0c1536f1afd41668d39d1d13984bcfb21
aziz <aracsa.aziz@gmail.com>
2021-03-29 17:55:28 +0100

    1st commit

--- ex1.py
+++ ex1.py
@@ -1,16 +1,4 @@
 # python3
-
-from collections import namedtuple
-
-Bracket = namedtuple("Bracket", ["char", "position"])
-
-
-def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
-
-
-def find_mismatch(text):# python3
-
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
@@ -47,31 +35,10 @@ def find_mismatch(text):
     else:
           return 'Success'
 
-
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
     print(mismatch)
-
-if __name__ == "__main__":
-    main()
-
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-
-
-def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
 
 
 if __name__ == "__main__":
